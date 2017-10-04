ee68182b8866f9e4a17e2f620104b2dac0533f39
hooo <dragonahooo@hotmail.com>
2017-10-05 05:26:01 +0800

    按钮事件

--- app/src/main/java/com/ho/mycheckbook/MainActivity.java
+++ app/src/main/java/com/ho/mycheckbook/MainActivity.java
@@ -2,12 +2,159 @@ package com.ho.mycheckbook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn0;
+    private Button btn1;
+    private Button btn2;
+    private Button btn3;
+    private Button btn4;
+    private Button btn5;
+    private Button btn6;
+    private Button btn7;
+    private Button btn8;
+    private Button btn9;
+    private Button btnDOT;
+    private Button btnBACKSPACE;
+    private Button btnCancel;
+    private TextView input;
+
+    private String _numStr = "0";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn0 = (Button)findViewById(R.id.btn0);
+        btn1 = (Button)findViewById(R.id.btn1);
+        btn2 = (Button)findViewById(R.id.btn2);
+        btn3 = (Button)findViewById(R.id.btn3);
+        btn4 = (Button)findViewById(R.id.btn4);
+        btn5 = (Button)findViewById(R.id.btn5);
+        btn6 = (Button)findViewById(R.id.btn6);
+        btn7 = (Button)findViewById(R.id.btn7);
+        btn8 = (Button)findViewById(R.id.btn8);
+        btn9 = (Button)findViewById(R.id.btn9);
+        btnDOT = (Button)findViewById(R.id.btnDOT);
+        btnBACKSPACE = (Button)findViewById(R.id.btnBACKSPACE);
+
+        input = (TextView)findViewById(R.id.input);
+        btnCancel = (Button)findViewById(R.id.btnCancelInput);
+        btnCancel.setVisibility(View.INVISIBLE);
+
+        clickListener();
+
+
+    }
+
+    public void clickListener()
+    {
+        btn0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addNum("0");
+            }
+        });
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addNum("1");
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addNum("2");
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addNum("3");
+            }
+        });
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addNum("4");
+            }
+        });
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addNum("5");
+            }
+        });
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addNum("6");
+            }
+        });
+        btn7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addNum("7");
+            }
+        });
+        btn7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addNum("7");
+            }
+        });
+        btn8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addNum("8");
+            }
+        });
+        btn9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addNum("9");
+            }
+        });
+
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setNumber(0.0);
+            }
+        });
+
+    }
+
+    private void setNumber(Double num)
+    {
+        if(num == 0 || num == 0.0)
+        {
+            initUI();
+        }
+
+
+    }
+
+    private void initUI()
+    {
+        _numStr = "0";
+        input.setText(_numStr);
+        btnCancel.setVisibility(View.INVISIBLE);
+    }
+
+
+    private void addNum(String str)
+    {
+        _numStr += str;
+
+        input.setText(_numStr);
+        btnCancel.setVisibility(View.VISIBLE);
     }
 }
